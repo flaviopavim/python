90af2e60fa900e03b99ad036791aab01779c0aa1
Flávio Pavim <contato@flaviopavim.com.br>
2025-01-19 15:43:40 -0300

    Fixes

--- Logics/Secret Friend.py
+++ Logics/Secret Friend.py
@@ -1,8 +1,11 @@
-#sorteio de amigo secreto
+# Secret Santa draw script
+# This script randomly shuffles a list of names and assigns each person to another as their "Secret Santa."
+# The last person in the shuffled list is assigned to the first person, ensuring a complete loop.
 
 import random
 
-amigos=[
+# List of participants in the Secret Santa
+amigos = [
     'flavio',
     'leo',
     'patrick',
@@ -13,10 +16,13 @@ amigos=[
     'hasan'
 ]
 
+# Shuffle the list randomly to ensure fairness
 random.shuffle(amigos)
 
+# Assign each person a recipient
 for contador_1 in range(len(amigos)):
-    contador_2=contador_1+1
-    if (contador_2==len(amigos)):
-        contador_2=0
-    print(amigos[contador_1]+' -> '+amigos[contador_2])
+    contador_2 = contador_1 + 1  # Next person in the list
+    if contador_2 == len(amigos):  # Wrap around to the first person if at the end of the list
+        contador_2 = 0
+    # Print the pair: giver -> recipient
+    print(amigos[contador_1] + ' -> ' + amigos[contador_2])
