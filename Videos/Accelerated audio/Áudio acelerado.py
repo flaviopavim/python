ad25b8a4a518088d5459a4ba39dd7c8e18442c98
Flávio Pavim <contato@flaviopavim.com.br>
2025-01-21 20:44:54 -0300

    Updates

--- Videos/Accelerated audio/Áudio acelerado.py
+++ Videos/Accelerated audio/Áudio acelerado.py
@@ -2,40 +2,43 @@ import os
 from gtts import gTTS
 from moviepy.editor import *
 
-texto=""
+texto = ""
 
-# Ler o conteúdo do arquivo de texto
+# Read the content of the "inicio.txt" file and append it to the text variable
 with open("inicio.txt", "r", encoding="utf-8") as file:
-    texto = texto+file.read()+".\n\n"
+    texto = texto + file.read() + ".\n\n"
+
+# Read the content of the "texto.txt" file and append it to the text variable
 with open("texto.txt", "r", encoding="utf-8") as file:
-    texto = texto+file.read()+".\n\n"
+    texto = texto + file.read() + ".\n\n"
+
+# Read the content of the "final.txt" file and append it to the text variable
 with open("final.txt", "r", encoding="utf-8") as file:
-    texto = texto+file.read()
+    texto = texto + file.read()
 
-
-# Configurar as opções do gTTS
+# Configure the gTTS options
 tts = gTTS(text=texto, lang="pt-br", slow=False)
 
-# Salvar o áudio em um arquivo temporário
+# Save the audio to a temporary file
 temp_audio_file = "temp_audio.mp3"
 tts.save(temp_audio_file)
 
-# Obter a duração do áudio em segundos
+# Get the duration of the audio in seconds
 audio_clip = AudioFileClip(temp_audio_file)
 audio_duration = audio_clip.duration
 
-# Criar um vídeo preto com a duração do áudio
+# Create a black video with the same duration as the audio
 video = ColorClip((1280, 720), color=(0, 0, 0), duration=audio_duration)
 video_with_audio = video.set_audio(audio_clip)
 
-# Acelerar o vídeo
+# Speed up the video
 video_accelerated = video_with_audio.fx(vfx.speedx, factor=1.1)
 
-# Salvar o áudio acelerado em formato MP3
+# Save the accelerated audio in MP3 format
 output_file = "audio.mp3"
 video_accelerated.audio.write_audiofile(output_file, codec="mp3")
 
-# Remover o arquivo temporário de áudio
+# Remove the temporary audio file
 os.remove(temp_audio_file)
 
-print("Áudio acelerado salvo em formato MP3 com sucesso!")
+print("Accelerated audio successfully saved in MP3 format!")
