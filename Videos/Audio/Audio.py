1f052de36218448ea27bf633c29a2826fd0c86c5
Flávio Pavim <contato@flaviopavim.com.br>
2025-01-21 20:51:13 -0300

    Updates

--- Videos/Audio/Audio.py
+++ Videos/Audio/Audio.py
@@ -4,17 +4,24 @@ from PIL import Image
 import os
 import random
 
-diretorio = "./"
+directory = "./"
 
-for arquivo in os.listdir(diretorio):
-    if arquivo.endswith(".txt"):
-        nome_arquivo, extensao = os.path.splitext(arquivo)
-        with open(os.path.join(diretorio, arquivo), "r") as f:
-            conteudo = f.read()
-            print(nome_arquivo)
-            with open(diretorio+nome_arquivo+".txt", encoding='utf-8') as file:
-                texto = file.read()
-            print('Salvando audio')
-            tts = gTTS(text=texto, lang='pt-br', slow=False)
-            tts.save(nome_arquivo+'.mp3')
-            print('Áudio salvo')
+# Loop through the files in the directory
+for file in os.listdir(directory):
+    if file.endswith(".txt"):  # Check if the file has a .txt extension
+        file_name, extension = os.path.splitext(file)  # Split the file name and its extension
+        
+        # Open and read the content of the .txt file
+        with open(os.path.join(directory, file), "r") as f:
+            content = f.read()
+            print(file_name)
+            
+            # Read the text file content with UTF-8 encoding
+            with open(directory + file_name + ".txt", encoding='utf-8') as text_file:
+                text = text_file.read()
+            
+            # Generate audio from the text
+            print('Saving audio')
+            tts = gTTS(text=text, lang='pt-br', slow=False)
+            tts.save(file_name + '.mp3')  # Save the audio as an MP3 file
+            print('Audio saved')
