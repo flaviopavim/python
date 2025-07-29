d43379e7f08850d53466b488026dc88e99f95c00
Flávio Pavim <contato@flaviopavim.com.br>
2025-07-29 13:38:25 -0300

    🚀 Pushed some code! If it breaks, I was never here.

--- Crypto calc/min_sell.py
+++ Crypto calc/min_sell.py
@@ -7,23 +7,15 @@ sell_price = buy_price + (buy_price * ((tax/100) + (tax/100)))
 
 print(f"Sell price: R$ {sell_price:.2f}")
 
+######################################################################
+######################################################################
+######################################################################
 
-
-
-
-
-
-
-
-
-
-
-
-preco_de_compra = 658000
-taxa = 0.21 # porcentagem de taxa
-
-porcentagem_da_taxa = taxa / 100
-total_da_taxa = porcentagem_da_taxa * 2 #na compra e na venda
-
-preco_de_venda = preco_de_compra + (preco_de_compra * total_da_taxa)
-print(f"Preço de venda: R$ {preco_de_venda:.2f}")
+#preco_de_compra = 658000
+#taxa = 0.21 # porcentagem de taxa
+#
+#porcentagem_da_taxa = taxa / 100
+#total_da_taxa = porcentagem_da_taxa * 2 #na compra e na venda
+#
+#preco_de_venda = preco_de_compra + (preco_de_compra * total_da_taxa)
+#print(f"Preço de venda: R$ {preco_de_venda:.2f}")
